ac990525ced8b5558d0121abb86aafdcb63d515c
Giuliano Dallari <giuliano.dallari.larrosa@mi.unc.edu.ar>
2024-04-08 20:36:55 -0300

    Refactor: filter request in Python

--- library.c
+++ library.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-int hello(){
-    printf("    Hello world from c!\n");
-    return 0;
-}
-
 //Recibe el nombre del archivo de donde sacar los datos y el nombre del país para el cual realizar los calculos
-int calcGeany(char* requestPath, char* country){
+int calcGeany(float value){
     //Do nothing
-    printf("    I  received '%s' and '%s'.\n",requestPath,country);
+    printf("    I  received '%f'.\n",value);
     return 0;
 }
--- main.py
+++ main.py
@@ -1,41 +1,51 @@
 import requests
 import sys
 import ctypes
+import json
+import numbers
 
 def main(libraryName):
-    respose_path="response.txt"
-    """
+    ""
     #Indice de todos los paises
-    #url = "https://api.worldbank.org/v2/en/country/all/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=32500&page=1&country=%22Argentina%22"
+    url = "https://api.worldbank.org/v2/en/country/all/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=32500&page=1&country=%22Argentina%22"
     #Indice de Argentina
-    url = "https://api.worldbank.org/v2/en/country/ARG/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=32500&page=1"
+    #url = "https://api.worldbank.org/v2/en/country/ARG/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=32500&page=1"
     
 
     resp = requests.get(url)
 
     # Verificar si la solicitud fue exitosa
     if resp.status_code == 200:
-        print(f"Request exitosa, guardando la respuesta en '{respose_path}'.")
-        with open("response.txt","w") as file:
-            file.write(resp.text)
+        print(f"Request exitosa.")
     else:
         # Imprimir un mensaje de error si la solicitud falló
         print("Error al realizar la solicitud:", resp.status_code)
         sys.exit(1)
-    """
+    ""
+    data =json.loads(resp.text)
+    
+    #desiredCountry = input("De que país desea conocer el indice gini? ")
+    desiredCountry= "Argentina"
+
+    giniValues = [(entry["date"], entry["value"]) for entry in data[1] if entry["country"]["value"] == desiredCountry]
+
+    print(f"Valores numéricos para {desiredCountry}(año, valor):", giniValues)
+
     # Carga la librería
     library = ctypes.CDLL(libraryName)
 
     # Define los argumentos y retornos de la función de la librería
-    library.calcGeany.argtypes = (ctypes.c_char_p, ctypes.c_char_p)
+    library.calcGeany.argtypes = [ctypes.c_float]
     library.calcGeany.restype = ctypes.c_int
 
     # Define un wrapper para llamar a la función de C
-    def calcGeany(requestPath, country):
-        return library.calcGeany(requestPath.encode(), country.encode())
+    def calcGeany(value):
+        return library.calcGeany(value)
 
-    #ToDo: pedir al usuario el país a filtrar
-    print("Retorno de la función: ",calcGeany(respose_path,"Argentina"))
+    for _, value in giniValues:
+        if isinstance(value, numbers.Number):
+            print(calcGeany(value))
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
